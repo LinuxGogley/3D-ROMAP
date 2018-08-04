7e0a70aacdc501a311dbf2aa1468f877fb56cb30
LinuxGogley <liam.gogley@aol.com>
2018-08-04 10:01:57 -0700

    ICP edit

--- Software/PCL-Mapping/Iterative-Closest-Point/iterative_closest_point.cpp
+++ Software/PCL-Mapping/Iterative-Closest-Point/iterative_closest_point.cpp
@@ -9,42 +9,18 @@ int main (int argc, char** argv)
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // Fill in the CloudIn data
- if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("Concat.pcd", *cloud_in) == -1) //* load the file
-  {
-    PCL_ERROR ("Couldn't read file Concat.pcd \n");
-    return (-1);
-  }
-  std::cout << "Loaded "
-            << cloud->width * cloud->height
-            << " data points from test_pcd.pcd with the following fields: "
-            << std::endl;
-  for (size_t i = 0; i < cloud_in->points.size(); ++i)
-    std::cout << "    " << cloud_in->points[i].x
-              << " "    << cloud_in->points[i].y
-              << " "    << cloud_in->points[i].z << std::endl;
-
-  std::cout << "Saved " << cloud_in->points.size () << " data points to input:"
-      << std::endl;
-  for (size_t i = 0; i < cloud_in->points.size (); ++i) std::cout << "    " <<
-      cloud_in->points[i].x << " " << cloud_in->points[i].y << " " <<
-      cloud_in->points[i].z << std::endl;
-  *cloud_out = *cloud_in;
-  std::cout << "size:" << cloud_out->points.size() << std::endl;
-  for (size_t i = 0; i < cloud_in->points.size (); ++i)
-    cloud_out->points[i].x = cloud_in->points[i].x + 0.7f;
-  std::cout << "Transformed " << cloud_in->points.size () << " data points:"
-      << std::endl;
-  for (size_t i = 0; i < cloud_out->points.size(); ++i)
-    std::cout << "    " << cloud_out->points[i].x << " " <<
-      cloud_out->points[i].y << " " << cloud_out->points[i].z << std::endl;
-  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  pcl::io::loadPCDFile<pcl::PointXYZRGB> ("Example1.pcd", *cloud_in);
+  pcl::io::loadPCDFile<pcl::PointXYZRGB> ("Example2.pcd", *cloud_out);
+  
+  pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
   icp.setInputSource(cloud_in);
   icp.setInputTarget(cloud_out);
-  pcl::PointCloud<pcl::PointXYZ> Final;
+  pcl::PointCloud<pcl::PointXYZRGB> Final;
   icp.align(Final);
   std::cout << "has converged:" << icp.hasConverged() << " score: " <<
   icp.getFitnessScore() << std::endl;
   std::cout << icp.getFinalTransformation() << std::endl;
+  pcl::io::savePCDFileBinary ("Final.pcd", Final);
 
  return (0);
 }
