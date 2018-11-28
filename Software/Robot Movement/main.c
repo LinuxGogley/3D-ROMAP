af2d4d0164898cce997d255edffae402a36c0737
Mark Munoz <36170296+MarkMunoz1096@users.noreply.github.com>
2018-11-28 00:06:53 -0800

    Fixed error caused by fractions

--- Software/Robot Movement/main.c
+++ Software/Robot Movement/main.c
@@ -31,14 +31,14 @@ int main(void){
 	UART1_Init();             // HC05 UART 57600 baud, 8O1 
 	PortF_Init();             // debug LEDs
 	Direction_Init();         // Init PD0 and PD1 as motor direction signals, PD2 as output enable
-  GPIO_PORTD_DATA_R |= 0x04;// enables motors output
+    GPIO_PORTD_DATA_R |= 0x04;// enables motors output
 	PWM0A_Init(period, 6);    // initialize PB4 as motor 1 PWM(initially off)
 	PWM0B_Init(period, 6);    // initialize PB5 as motor 2 PWM(initially off)
 	dutyMax = period/2;       // initially 50% speed
 
 	//**************Configure bluetooth Commands*************
 	//UART1_OutString("AT+NAME=3D-ROMAP\r\n");     // set custom name	
-  //UART1_OutString("AT+PSWD=0159\r\n");         // set custom password
+    //UART1_OutString("AT+PSWD=0159\r\n");         // set custom password
 	//UART1_OutString("AT+UART=57600,0,1\r\n");    // 57600, 8O1
 
 	while(1){
@@ -93,24 +93,24 @@ int main(void){
 			GPIO_PORTF_DATA_R &= ~0x0E; // clear debug LEDs
 		}
 		
-		PWM0A_Duty(dutyA*(2/3));      // update motor PWMs
-		PWM0B_Duty(dutyB*(2/3));	    // run at max 12v (2/3 of 18v)
+		PWM0A_Duty(dutyA*.666666667);    // update motor PWMs
+		PWM0B_Duty(dutyB*.666666667);	// run at max 12v (2/3 of 18v)
 	}
 }
 
 // 4. Subroutines Section
 // DEBUG LEDs
 void PortF_Init(void){ 
-	// GPIO assignments
-	volatile unsigned long delay;     // dummy instruction variable
-	SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  // GPIO assignments
+  volatile unsigned long delay;     // dummy instruction variable
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
   delay = SYSCTL_RCGC2_R;           // dummy instruction to give setup enough time   
   GPIO_PORTF_LOCK_R  |=  0x4C4F434B;// 2) unlock PortF PF0  
   GPIO_PORTF_CR_R    |=  0x0E;      // allow changes to PF4-PF1     
   GPIO_PORTF_AMSEL_R &= ~0x0E;      // 3) disable analog function
   GPIO_PORTF_PCTL_R  &= ~0x0000FFF0;// 4) GPIO clear bit PCTL  
-	GPIO_PORTF_DIR_R   |=  0x0E;      //    PF3-PF1 is output
+  GPIO_PORTF_DIR_R   |=  0x0E;      //    PF3-PF1 is output
   GPIO_PORTF_AFSEL_R &= ~0x0E;      // 6) no alternate function on PF4-PF1
-	GPIO_PORTF_PUR_R   &= ~0x0E;      //    disable pullup resistors for LED
+  GPIO_PORTF_PUR_R   &= ~0x0E;      //    disable pullup resistors for LED
   GPIO_PORTF_DEN_R   |=  0x0E;      // 7) enable digital pins PF4-PF1 
-	}
+  }
