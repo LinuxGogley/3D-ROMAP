1acc4c5d55e4413110de217abc3a27c0e3f9a6f7
LinuxGogley <liam.gogley@aol.com>
2018-12-10 08:41:06 -0800

    Script launches child processes

--- romap_launch.sh
+++ romap_launch.sh
@@ -6,7 +6,7 @@ export ROS_HOSTNAME=$HOSTNAME
 
 while true
 do
-HEIGHT=15
+HEIGHT=11
 WIDTH=40
 CHOICE_HEIGHT=5
 BACKTITLE="3D-ROMAP Launcher"
@@ -37,12 +37,12 @@ clear
             xterm -bg black -fg white -hold -e sshpass -f <(printf '%s\n' $JETSON) ssh -l nvidia tegra-ubuntu "roscore" &
             ;;
         3)
-            xterm -bg black -fg white -hold -e sshpass -f <(printf '%s\n' $JETSON) ssh -l nvidia tegra-ubuntu "roslaunch realsense2_ros rs_rgbd.launch" ;
-            xterm -bg black -fg white -hold -e "roslaunch rtabmap_ros rtabmap.launch" ;
-            xterm -bg black -fg white -hold -e "roslaunch hectorslam_ros ROMAP.launch"
+            xterm -bg black -fg white -hold -e sshpass -f <(printf '%s\n' $JETSON) ssh -l nvidia tegra-ubuntu "roslaunch realsense2_ros rs_rgbd.launch" &
+            xterm -bg black -fg white -hold -e "roslaunch rtabmap_ros rtabmap.launch" &
+            xterm -bg black -fg white -hold -e "roslaunch hectorslam_ros ROMAP.launch" &
             ;;
         4)
-            xterm -bg black -fg white -hold -e sshpass -f <(printf '%s\n' $JETSON) ssh nvidia@tegra-ubuntu 
+            xterm -bg black -fg white -hold -e sshpass -f <(printf '%s\n' $JETSON) ssh nvidia@tegra-ubuntu &
             ;;
         5)  export JETSON=$NULL ;
             break
